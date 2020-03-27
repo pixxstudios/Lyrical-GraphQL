8f63140ae8377c2da013e86f99a4c6c7b1264786
Gagandeep Singh <gagan.ddun@gmail.com>
2020-03-27 21:34:23 +1100

    fixed db connection

--- server/server.js
+++ server/server.js
@@ -8,16 +8,23 @@ const schema = require('./schema/schema');
 const app = express();
 
 // Replace with your mongoLab URI
-const MONGO_URI = '';
+const MONGO_URI = 'mongodb://pixxstudios:pixxstudios1@ds223812.mlab.com:23812/lyricaldb99';
 if (!MONGO_URI) {
   throw new Error('You must provide a MongoLab URI');
 }
 
 mongoose.Promise = global.Promise;
-mongoose.connect(MONGO_URI);
-mongoose.connection
-    .once('open', () => console.log('Connected to MongoLab instance.'))
-    .on('error', error => console.log('Error connecting to MongoLab:', error));
+// mongoose.connect(MONGO_URI);
+mongoose.createConnection(MONGO_URI, { useNewUrlParser: true })
+    .then(client => {
+        // _db = client;
+        console.log('connected ... ', client);
+        // callback();
+    })
+    .catch(err => console.log('Error ', err));
+/* mongoose.connection
+    .once('openUri', () => console.log('Connected to MongoLab instance.'))
+    .on('error', error => console.log('Error connecting to MongoLab:', error)); */
 
 app.use(bodyParser.json());
 app.use('/graphql', expressGraphQL({
